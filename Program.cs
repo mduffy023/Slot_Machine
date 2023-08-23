f6176ee1a0cfef91548ad0e1984d13adfa8beb9d
martin <martinduffy@hotmail.co.uk>
2023-08-23 21:47:11 +0100

    added an output of the array using two for loops

--- Program.cs
+++ Program.cs
@@ -17,6 +17,18 @@
             slots[0, 2] = 0;
             slots[1, 2] = 2;
             slots[2, 2] = 1;
+
+            //loops through each row of the slots array 
+            for (int i = 0; i < slots.GetLength(0); i++)
+            {
+                //loops through each colum of the slots array for the curret row 
+                for (int j = 0; j < slots.GetLength(1); j++)
+                {
+                    Console.Write(slots[i, j] + " ");
+                }
+              Console.WriteLine();
+            }
+
         }
     }
 }
