fe7ed4ccdb0109e33412ec6873513c0985fac939
martin <martinduffy@hotmail.co.uk>
2023-08-24 18:43:35 +0100

    add  a shifting function to the slots

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Slot_Machine
+﻿using System;
+using System.Threading;
+
+namespace Slot_Machine
 {
     internal class Program
     {
@@ -18,17 +21,41 @@
             slots[1, 2] = 2;
             slots[2, 2] = 1;
 
-            //loops through each row of the slots array 
-            for (int i = 0; i < slots.GetLength(0); i++)
-            {
-                //loops through each colum of the slots array for the curret row 
-                for (int j = 0; j < slots.GetLength(1); j++)
-                {
-                    Console.Write(slots[i, j] + " ");
-                }
-              Console.WriteLine();
-            }
+            Random  rand = new Random();
 
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    Console.ReadKey(true);
+                    break;
+                }
+
+
+                //loops through each row of the slots array
+                for (int i = 0; i < slots.GetLength(0); i++)
+                {
+                    //loops through each colum of the slots array for the curret row 
+                    for (int j = 0; j < slots.GetLength(0); j++)
+                    {
+                        //shift the nums between 0, 1, 2
+                        int shiftNums = rand.Next(0, 3);
+                        slots[i, j] = (slots[i,j] + shiftNums) % 3 ;                   
+                    }
+
+                    Console.Clear();
+                }
+             
+                for (int i = 0; i < slots.GetLength(1); i++)
+                {
+                    for (int j = 0; j < slots.GetLength(1); j++)
+                    {
+                        Console.Write(slots[i, j] + " ");
+                    }
+                      Console.WriteLine();
+                }
+                Thread.Sleep(1000);
+            }          
         }
     }
 }
