747fe9a1b98ba2424381cc8ee41425c4da27837f
martin <martinduffy@hotmail.co.uk>
2023-08-31 20:04:38 +0100

    added a if to check if the middle row are all the same

--- Program.cs
+++ Program.cs
@@ -19,41 +19,57 @@ namespace Slot_Machine
 
             Money = Convert.ToInt32(Console.ReadLine());
 
-            while (true)
-            {
+            while (Money > 0)
+            {    
              
-                    Console.WriteLine($"Balance ${Money}");
+                
+             Console.WriteLine($"Balance ${Money}");
 
-                    Console.WriteLine($"would you like to play? press enter");
+             Console.WriteLine($"would you like to play? press enter to continue");
 
-                 if (Console.ReadKey(true).Key != ConsoleKey.Enter)
-                 {
-                  break;
-                 }
+            if (Console.ReadKey(true).Key != ConsoleKey.Enter)
+               {
+                 break;
+               }
 
-                  Money--;
+               Money--;
                   
                 //loops through each row of the slots array
-                for (int i = 0; i < slots.GetLength(0); i++)
+            for (int i = 0; i < slots.GetLength(0); i++)
+            {
+                 //loops through each colum of the slots array for the curret row 
+                for (int j = 0; j < slots.GetLength(0); j++)
                 {
-                    //loops through each colum of the slots array for the curret row 
-                    for (int j = 0; j < slots.GetLength(0); j++)
-                    {
                         //shift the nums between 0, 1, 2
                         int shiftNums = rand.Next(0, 3);
                         slots[i, j] = (slots[i,j] + shiftNums) % 3 ;                   
-                    }
-
-                    Console.Clear();
                 }
+
+                Console.Clear();
+            }
              
-                for (int i = 0; i < slots.GetLength(1); i++)
+            for (int i = 0; i < slots.GetLength(1); i++)
+            {
+               for (int j = 0; j < slots.GetLength(1); j++)
+               {
+
+                Console.Write(slots[i, j] + " ");
+
+               }
+
+                Console.WriteLine();
+            }
+
+                if (slots[1,0] == slots[1,1] && slots[1,1] == slots[1,2])
                 {
-                    for (int j = 0; j < slots.GetLength(1); j++)
-                    {
-                        Console.Write(slots[i, j] + " ");
-                    }
-                      Console.WriteLine();
+                    Console.Write("YOU WIN!");
+                    Money++;
+                }
+                if(Money == 0)
+                {
+                    Console.Write("Would you like to insert more money or preess any key to exit");
+
+                    Money = Convert.ToInt32(Console.ReadLine());
                 }
                 Thread.Sleep(500);
             }          
