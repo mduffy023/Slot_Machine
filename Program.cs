aab742c954d5340e1cd2e629ad46db760068cb24
martin <martinduffy@hotmail.co.uk>
2023-09-26 20:27:06 +0100

    #13 madde a game description
    #12 changed some logic still need some work

--- Program.cs
+++ Program.cs
@@ -11,6 +11,11 @@ namespace Slot_Machine
             const int COLUMNS = 3;
 
             Console.WriteLine("Slot Machine Game");
+            Console.WriteLine("Game Rules:");
+            Console.WriteLine("1. The cost is $1 for 1 line, $2 for 2 lines, and $3 for 3 lines.");
+            Console.WriteLine("2. You can choose to play Horizontal, Vertical, or Diagonal lines.");
+            Console.WriteLine("3. If the line matches, you win the amount equivalent to the number of lines played.");
+            Console.WriteLine("4. Press Enter to spin.");
 
             int[,] slots = new int[ROWS, COLUMNS];
             Random rand = new Random();
@@ -21,115 +26,190 @@ namespace Slot_Machine
             while (money > 0)
             {
                 Console.WriteLine($"Balance ${money}");
-                Console.WriteLine($"Press Enter to start spin");
 
+                char lineType;
                 while (true)
                 {
-                    ConsoleKey pressedKey = Console.ReadKey(true).Key;
-
-                    if (pressedKey == ConsoleKey.Enter)
-                    {
+                    Console.WriteLine("Choose the line type you would like to play: H for Horizontal, V for Vertical, and D for Diagonal");
+                    lineType = char.ToUpper(Console.ReadKey(true).KeyChar);
+                    if (lineType == 'H' || lineType == 'V' || (lineType == 'D' && ROWS >= 2))
                         break;
-                    }
-                    else 
-                    {
-                        Console.WriteLine("Please only press Enter.");                     
-                    }
-                }
-                money--;
-
-                //loops through each row of the slots array
-                for (int i = 0; i < slots.GetLength(0); i++)
-                {
-                    //loops through each colum of the slots array for the curret row 
-                    for (int j = 0; j < slots.GetLength(1); j++)
-                    {
-                        //shift the nums between 0, 1, 2
-                        int shiftNums = rand.Next(0, ROWS);
-                        slots[i, j] = (slots[i, j] + shiftNums) % 3;
-                    }
-                    Console.Clear();
+                    else
+                        Console.WriteLine("Invalid input. Please enter H, V, or D.");
                 }
 
-                for (int i = 0; i < slots.GetLength(0); i++)
+                Console.WriteLine($"Choose the number of lines you would like to play (1 to {Math.Min(ROWS, COLUMNS)}) ");
+                int lineCount;
+                while (true)
                 {
-                    for (int j = 0; j < slots.GetLength(1); j++)
+                    try
                     {
-
-                        Console.Write(slots[i, j] + " ");
-
-                    }
-
-                    Console.WriteLine();
-                }
-
-                //row check
-                for (int indexRow = 0; indexRow < slots.GetLength(0); indexRow++)
-                {
-                    int refValue = slots[indexRow, 0];
-                    bool isMatchingRow = true;
-
-                    for (int indexColumn = 0; indexColumn < slots.GetLength(1); indexColumn++)            
-                    {
-                        if(slots[indexRow, indexColumn] != refValue)
+                        lineCount = Convert.ToInt32(Console.ReadLine());
+                  
+                        if (lineCount >= 1 && lineCount <= Math.Min(ROWS, COLUMNS) && lineCount <= money)
                         {
-                            isMatchingRow = false;
-                            break;
-                        }                    
-                    }                  
-                    if (isMatchingRow) 
-                    {
-                        Console.WriteLine($"You WIN! Row matches on {(indexRow+1)}");
-                        money ++;
-                    }
-                }
-
-                //column check
-                for (int indexColumn = 0; indexColumn < COLUMNS; indexColumn++)
-                {
-                    int refValue = slots[indexColumn, 0];
-                    bool isMatchingColumn = true;
-                    for (int indexRow = 0; indexRow < ROWS; indexRow++)
-                    {
-                        if (slots[indexRow, indexColumn] != refValue)
-                        {
-                            isMatchingColumn = false;
                             break;
                         }
+                        else
+                        {
+                            Console.WriteLine($"Invalid input. Please enter a number 1 to {Math.Min(ROWS, COLUMNS)}, and ensure you have enough balance.");
+                        }
                     }
-
-                    if(isMatchingColumn)
+                    catch 
                     {
-                        Console.WriteLine($"You WIN! Columm matches on {(indexColumn + 1)}");
-                        money++;
+                        Console.WriteLine("Please enter a valid intger.");
                     }
                 }
-                //main diagonal checks 
-                bool MainDiagonalMatch = true;
-                int mainDigonalRef = slots[0, 0];
+
+                Console.WriteLine("Press Enter to start spin");
+                while (Console.ReadKey(true).Key != ConsoleKey.Enter) { Console.WriteLine("Please only press Enter."); }
+
+                money -= lineCount;
+
+                // Generate slot values
                 for (int indexRow = 0; indexRow < ROWS; indexRow++)
                 {
-                    if (slots[indexRow, indexRow] != mainDigonalRef)
+                    for (int indexCol = 0; indexCol < COLUMNS; indexCol++)
                     {
-                        MainDiagonalMatch = false;
-                        break;
+                        slots[indexRow, indexCol] = rand.Next(1, 4);
                     }
                 }
 
-                if (MainDiagonalMatch)
+                int winnings = 0;
+
+                // Check the selected lines for matches and calculate winnings
+                if (lineType == 'H')
                 {
-                   Console.WriteLine("You WIN! Main Diagonal Matches!");
-                    money++;
+                    for (int indexRow = 0; indexRow < lineCount; indexRow++)
+                    {
+                        bool allEqual = true;
+                        for (int indexCol = 0; indexCol < COLUMNS - 1; indexCol++)
+                        {
+                            if (slots[indexRow, indexCol] != slots[indexRow, indexCol + 1])
+                            {
+                                allEqual = false;
+                                break;
+                            }
+                        }
+                        if (allEqual)
+                        {
+                            winnings += lineCount;
+                        }
+                    }
                 }
+                else if (lineType == 'V')
+                {
+                    for (int indexCol = 0; indexCol < lineCount; indexCol++)
+                    {
+                        bool allEqual = true;
+                        for (int indexRow = 0; indexRow < ROWS - 1; indexRow++)
+                        {
+                            if (slots[indexRow, indexCol] != slots[indexRow + 1, indexCol])
+                            {
+                                allEqual = false;
+                                break;
+                            }
+                        }
+                        if (allEqual)
+                        {
+                            winnings += lineCount;
+                        }
+                    }
+                }
+                else 
+                {
+                    if (lineCount >= 2)
+                    {
+                        // Check both diagonals for a match
+                        bool firstDiagonalEqual = true;
+                        bool secondDiagonalEqual = true;
+                        for (int i = 0; i < ROWS - 1; i++)
+                        {
+                            if (slots[i, i] != slots[i + 1, i + 1])
+                            {
+                                firstDiagonalEqual = false;
+                            }
+                            if (slots[i, COLUMNS - 1 - i] != slots[i + 1, COLUMNS - 2 - i])
+                            {
+                                secondDiagonalEqual = false;
+                            }
+                        }
+                        if (firstDiagonalEqual || secondDiagonalEqual)
+                        {
+                            winnings += lineCount;
+                        }
+                    }
+                }
+
+                money += winnings;
+
+                // Display the selected lines and the winnings
+                DisplaySelectedLines(slots, lineType, lineCount);
+                Console.WriteLine($"You have won ${winnings}. Current Balance: ${money}");
 
                 if (money == 0)
                 {
-                    Console.WriteLine("YOU LOSE! Would you like to insert more money or preess any key to exit");
-
-                    money = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("You Lose! Would you like to insert more money or press any key to exit?");
+                    if (int.TryParse(Console.ReadLine(), out int additionalMoney))
+                    {
+                        money += additionalMoney;
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
+
                 Thread.Sleep(500);
             }
         }
-    }
-}
+
+        // Helper method to display the selected lines
+        static void DisplaySelectedLines(int[,] slots, char lineType, int lineCount)
+        {
+            int ROWS = slots.GetLength(0);
+            int COLUMNS = slots.GetLength(1);
+
+            if (lineType == 'H')
+            {
+                for (int indexRow = 0; indexRow < lineCount; indexRow++)
+                {
+                    for (int indexCol = 0; indexCol < COLUMNS; indexCol++)
+                    {
+                        Console.Write(slots[indexRow, indexCol] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else if (lineType == 'V')
+            {
+                for (int indexRow = 0; indexRow < ROWS; indexRow++)
+                {
+                    for (int indexCol = 0; indexCol < lineCount; indexCol++)
+                    {
+                        Console.Write(slots[indexRow, indexCol] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            //else // Diagonal
+            //{
+            //    for (int i = 0; i < ROWS; i++)
+            //    {
+            //        for (int j = 0; j < COLUMNS; j++)
+            //        {
+            //            if (i == j || i + j == COLUMNS - 1)
+            //            {
+            //                Console.Write(slots[i, j] + " ");
+            //            }
+            //            else
+            //            {
+            //                Console.Write("  ");
+            //            }
+            //        }
+                    Console.WriteLine();
+                }
+            }
+        }
+//    }
+//}
